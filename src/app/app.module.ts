4469eb9a2a91fe947025f59183b95d5fb99406dc
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2019-10-23 21:50:33 +0700

    add config class for serving API URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Config } from './config';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -18,7 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Config
   ],
   bootstrap: [
     AppComponent
